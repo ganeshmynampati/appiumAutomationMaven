f991172d12f10b2624ad8498001164f068cd9aa6
ganeshmynampati <47498914+ganeshmynampati@users.noreply.github.com>
2021-07-09 15:47:24 +0530

    first commit
    first commit

---
+++ src/test/java/scenarios/BaseTest.java
@@ -0,0 +1,39 @@
+package scenarios;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class BaseTest {
+
+	static AndroidDriver<AndroidElement> driver = null;
+	static File appDir;
+	static File app;
+	static DesiredCapabilities cap;
+
+	public static AndroidDriver<AndroidElement> getCapabilities(String platformName, String version, String appName,
+			String deviceName, boolean isAndroid) throws MalformedURLException {
+		appDir = new File("src/test/java");
+		app = new File(appDir, appName);
+		cap = new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, platformName);
+		cap.setCapability(MobileCapabilityType.VERSION, version);
+		cap.setCapability("autoGrantPermissions", true);
+		cap.setCapability("noReset", "false");
+		if (isAndroid) {
+			cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Device");
+		} else {
+			cap.setCapability(MobileCapabilityType.DEVICE_NAME, deviceName);
+		}
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+		cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+		return driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+	}
+
+}
---
+++ src/test/java/scenarios/ShopBrand.java
@@ -0,0 +1,51 @@
+package scenarios;
+
+import java.net.MalformedURLException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class ShopBrand extends BaseTest {
+
+	static AndroidDriver<AndroidElement> driver = null;
+
+	@BeforeMethod
+	public void setUp() throws MalformedURLException {
+		driver = getCapabilities("Android", "10.0", "General-Store.apk", null, true);
+		System.out.println("Successful");
+	}
+
+	@Test(enabled = false)
+	public void login() {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//*[@resource-id='android:id/text1']")).click();
+		driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"India\"))");
+		List<AndroidElement> countryList = driver.findElements(By.className("android.widget.TextView"));
+		for (AndroidElement country : countryList)
+			if (country.getText().equals("India")) {
+				country.click();
+				break;
+			}
+		driver.findElement(By.xpath("//*[@resource-id='com.androidsample.generalstore:id/nameField']"))
+				.sendKeys("testUser");
+		driver.findElement(By.xpath("//*[@resource-id='com.androidsample.generalstore:id/radioFemale']")).click();
+		driver.findElement(By.xpath("//*[@resource-id='com.androidsample.generalstore:id/btnLetsShop']")).click();
+	}
+
+	@Test
+	public void verifyToastMessage() {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//*[@resource-id='com.androidsample.generalstore:id/btnLetsShop']")).click();
+		String toastPrompt = driver.findElement(By.xpath("//android.widget.Toast[1]")).getAttribute("name");
+		Assert.assertEquals(toastPrompt, "Please enter your name");
+
+	}
+
+}
